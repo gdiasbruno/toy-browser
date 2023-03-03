494faf86d096a308d05995696d6b3ba99d0e84ea
gdiasbruno <gdias.bruno@gmail.com>
2023-03-03 08:13:32 +0100

    Promisify the request method

--- index.ts
+++ index.ts
@@ -1,36 +1,55 @@
 import net from "net"
 
-const request = (url: string) => {
-    const [protocol, rest] = url.split("/")[0]
-    let [host, path] = rest.split("/")
-    path = "/" + path
+const request = (url: string): Promise<string[]> => {
+    return new Promise((resolve, reject) => {
+        const [protocol, rest] = url.split("/")[0]
+        console.log(rest)
+        let [host, path] = rest.split("/")
+        path = "/"
+        let result;
 
-    const socket = new net.Socket()
+        const socket = new net.Socket()
 
-    socket.connect(80, "example.org", () => {
-        console.log("Connected")
+        socket.connect(80, "example.org", () => {
+            console.log("Connected")
+            console.log(`GET ${path} HTTP/1.1\r\nHost: ${"example.org"}\r\n\r\n`)
 
-        socket.write(`GET ${path} HTTP/1.1\r\n`)
+            socket.write(`GET ${path} HTTP/1.1\r\nHost: ${"example.org"}\r\n\r\n`);
+        })
+
+        socket.on('data', (data) => {
+            console.log(`Received data from server: ${data}`);
+            result += data
+        });
+        
+        socket.on('close', () => {
+            console.log('Connection closed.');
+            
+        });
+
+        socket.on('error', (err) => {
+            console.error(`Error: ${err}`);
+            reject(err)
+        })
+
+        socket.on('end', () => {
+            console.log('Socket ended');
+            resolve(["headers", result])
+        })
     })
 
-    socket.on('data', (data) => {
-        console.log(`Received data from server: ${data}`);
-      });
-      
-    socket.on('close', () => {
-        console.log('Connection closed.');
-    });
-
-    return ["headers", "body"]
+    
 }
 
 const show = (body: string) => {
     console.log("BODY: " + body)
 }
 
-const load = (url: string) => {
-    const [headers, body] = request(url)
+const load = async (url: string) => {
+    const [headers, body] = await request(url);
     show(body)
 }
 
-load(process.argv[2])
+(async () => {
+    await load(process.argv[2])
+})()
